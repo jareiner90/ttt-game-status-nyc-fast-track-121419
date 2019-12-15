ad7425bebab796c5cd559e58db25756390a00073
John Reiner <johnreiner1990@gmail.com>
2019-12-15 16:37:54 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,8 +17,8 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.find do |combo|
-    position_taken?(board, combo[0]) && 
-    board[combo[0]] == board[1] && 
+    position_taken?(board, combo[0]) &&
+    board[combo[0]] == board[1] &&
     board[combo[0]] == board[2]
-  end 
+  end
 end 
